0d0dcf1b612c46990c77aa14ae2b0a13673ecd75
codeMatt91 <matteo.imbimbo@yahoo.it>
2021-12-21 14:42:55 +0100

    Created logical process

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('JS ok');
+
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+1- Chiedere all'utente i km percorsi
+2- chiedere l'età dell'utente
+3- Calcolo prezzo biglietto
+   3a- Se utente :
+      - minore di 18 anni (20%)
+      - maggiore di 65 anni (40%)
+4- Scrivo il prezzo approssimato a 2 cifre dopo la virgola
+
+*/
+
+
